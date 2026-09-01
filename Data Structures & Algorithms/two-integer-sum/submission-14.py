6418256dd24da8013371a436fdf960e8b13623e2
Sélim Ben Abdallah <137769705+selim-ba@users.noreply.github.com>
2026-09-01 22:43:45 +0200

    Add: two-integer-sum - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum/submission-14.py
@@ -0,0 +1,40 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        """
+        # Version 1 (65ms)
+        R = []
+        for i in range(0,len(nums)):
+            for j in range (i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    R.append(i)
+                    R.append(j)
+        return R
+        """
+
+        """
+        # Version 2 (85ms)
+        R = []
+        i = 0
+        j = 1
+        while (nums[i] + nums[j] != target) & (j <= len(nums)):
+            j += 1
+            if j == len(nums):
+                i += 1
+                j = i + 1
+        R.append(i)
+        R.append(j)
+        return R
+        """
+
+        # Version 3 - HashMap
+        prevMap =  {} # val : index
+        for i,n in enumerate(nums):
+            #print(i,n)
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff],i]
+            prevMap[n] = i
+        return
+
+        
